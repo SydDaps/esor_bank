3a6aa6c462993c58ec7a1bd9f565ccdba0cddb26
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2020-08-09 14:21:16 +0000

    changed naming convention to snake_case

--- customer_test.rb
+++ customer_test.rb
@@ -6,9 +6,9 @@ require_relative "customer"
 class TestCustomer < Minitest::Test
   def setup
     @fields = {
-      :fName => "sydney",
-      :lName => "Daps",
-      :phoneNumber => "0246188069",
+      :first_name => "sydney",
+      :last_name => "Daps",
+      :phone_number => "0246188069",
       :age => 23,
       :address => "23-455-445",
       :account => Account.new(:savings)
@@ -17,22 +17,26 @@ class TestCustomer < Minitest::Test
   end
 
   def test_initialize 
-    assert_equal @fields[:fName] , @customer.fName 
-    assert_equal @fields[:lName] , @customer.lName
+    assert_equal @fields[:first_name] , @customer.first_name 
+    assert_equal @fields[:last_name] , @customer.last_name
     assert_equal @fields[:age] , @customer.age
-    assert_equal @fields[:phoneNumber] , @customer.phoneNumber
+    assert_equal @fields[:phone_number] , @customer.phone_number
     assert_equal @fields[:address] , @customer.address
     assert_equal  @fields[:account], @customer.account[:savings]
   end
 
   def test_check_balance
     assert_equal @fields[:account].balance , @customer.balance(:savings)
-
-    @customer.account[:savings].depositFunds(90)
-    assert_equal 90 , @customer.balance(:savings)
-
-    @customer.account[:savings].withdrawFunds(40)
-    assert_equal 50 , @customer.balance(:savings)
   end
 
+  def test_check_balance_after_deposit
+    @customer.account[:savings].deposit_funds(90)
+    assert_equal 90 , @customer.balance(:savings)
+  end
+
+  def test_check_balance_after_withdrawal
+    @customer.account[:savings].deposit_funds(90)
+    @customer.account[:savings].withdraw_funds(40)
+    assert_equal 50 , @customer.balance(:savings)
+  end
 end
