ccc451eb8e40e807d7105fe24549b7c04d47eace
SydDaps <51008616+SydDaps@users.noreply.github.com>
2020-08-09 13:42:44 +0000

    Teller done  (#2)
    * Selase please check if the way i wrote the test is good practice
    
    * teller done and tested
    
    * changed naming convention to snake_case
    
    Co-authored-by: Selase Krakani <quajo@users.noreply.github.com>

---
+++ teller_test.rb
@@ -0,0 +1,25 @@
+require "minitest/autorun"
+require_relative "Teller"
+
+class TestTeller < Minitest::Test
+  
+  def setup
+    @fields = {
+      :first_name => "sydney",
+      :last_name => "Daps",
+      :phone_number => "0246188069",
+      :age => 23,
+      :address => "23-455-445"
+    }
+    @teller = Teller.new(@fields)
+  end
+
+  def test_initialize
+    assert_equal @fields[:first_name] , @teller.first_name 
+    assert_equal @fields[:last_name] , @teller.last_name
+    assert_equal @fields[:age] , @teller.age
+    assert_equal @fields[:phone_number] , @teller.phone_number
+    assert_equal @fields[:address] , @teller.address
+  end
+
+end
