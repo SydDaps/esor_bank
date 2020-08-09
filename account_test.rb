41376a9f04612f7ea67d0436b378b501c574aa98
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2020-08-09 13:34:17 +0000

    naming convention changes to snake_case

--- account_test.rb
+++ account_test.rb
@@ -11,13 +11,13 @@ class TestAccount < Minitest::Test
     assert_equal "savings" , @account.type
   end
 
-  def test_depositFunds
-    assert_equal 20.0, @account.depositFunds(20)
+  def test_deposit_funds
+    assert_equal 20.0, @account.deposit_funds(20)
   end
 
-  def test_withdrawFunds
+  def test_withdraw_funds
     @account.balance = 30
-    assert_equal 10.0, @account.withdrawFunds(20)
+    assert_equal 10.0, @account.withdraw_funds(20)
   end
 
 end
