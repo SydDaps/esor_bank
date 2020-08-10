993d8cd4673d92e850e843cbd33f63f2b510c3da
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2020-08-10 01:57:04 +0000

    Transaction done

---
+++ transaction.rb
@@ -0,0 +1,30 @@
+require_relative "customer"
+require_relative "Teller"
+
+class Transaction
+  attr_accessor :transaction_type, :customer, :teller, :transaction_amount, :account_type
+  def initialize(fields)
+    @transaction_type = fields[:type]
+    @customer = fields[:customer]
+    @teller = fields[:teller]
+    @transaction_amount = fields[:amount]
+    @account_type = fields[:account_type]
+  end
+
+  def do_transaction
+    if @transaction_type == "deposit"
+      @oldBalance = @customer.balance(@account_type)
+      @customer.account[@account_type].deposit_funds(@transaction_amount)
+    elsif @transaction_type == "withdrawal"
+      @oldBalance = @customer.balance(@account_type)
+      @customer.account[@account_type].withdraw_funds(@transaction_amount)
+    end
+
+    message = <<~MESSAGE
+      #{@transaction_type.capitalize} of GHC #{@transaction_amount.to_f} on #{@customer.first_name} #{@account_type} account 
+      completed by #{@teller.first_name}. New account balance : GHC #{@customer.balance(@account_type).to_f}
+    MESSAGE
+    
+    return message
+  end
+end
---
+++ transaction_test.rb
@@ -0,0 +1,68 @@
+require "minitest/autorun"
+require_relative "customer"
+require_relative "teller"
+require_relative "transaction"
+
+class TestCustomer < Minitest::Test
+  def setup
+    @customer_fields = {
+      :first_name => "Sydney",
+      :last_name => "Daps",
+      :phone_number => "0246188069",
+      :age => 23,
+      :address => "23-455-445",
+      :account => Account.new(:savings)
+    }
+
+    @teller_fields = {
+      :first_name => "Ama",
+      :last_name => "Yaaro",
+      :phone_number => "0246188069",
+      :age => 23,
+      :address => "23-455-445",
+    }
+
+    @transaction_details = {
+      :type => "withdrawal",
+      :customer => Customer.new(@customer_fields),
+      :teller  => Teller.new(@teller_fields),
+      :account_type => :savings,
+      :amount => 90
+    }
+
+    @transaction = Transaction.new(@transaction_details)
+  end
+
+  def test_initialize 
+    assert_equal @transaction_details[:type] , @transaction.transaction_type
+    assert_equal @transaction_details[:customer] , @transaction.customer
+    assert_equal @transaction_details[:teller] , @transaction.teller
+    assert_equal @transaction_details[:account_type] , @transaction.account_type
+    assert_equal @transaction_details[:amount] , @transaction.transaction_amount
+  end
+
+  def test_withdrawal
+    message = <<~MESSAGE
+      Withdrawal of GHC 90.0 on Sydney savings account 
+      completed by Ama. New account balance : GHC -90.0
+    MESSAGE
+
+    assert_equal message ,  @transaction.do_transaction
+  end
+
+  def test_deposit
+    @transaction.transaction_type = "deposit"
+    
+    message = <<~MESSAGE
+      Deposit of GHC 90.0 on Sydney savings account 
+      completed by Ama. New account balance : GHC 90.0
+    MESSAGE
+    
+    assert_equal message ,  @transaction.do_transaction
+  end
+
+
+
+
+
+end
