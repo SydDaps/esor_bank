da3b929c2511aabf2d50db49716cde3255e9b6ec
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2020-08-12 02:19:39 +0000

    Added Transfers and generate_message to transactions

--- transaction.rb
+++ transaction.rb
@@ -1,31 +1,47 @@
 require_relative "customer"
 require_relative "teller"
 
+
 class Transaction
-  attr_accessor :type, :customer, :teller, :amount, :account_type
+  attr_accessor :type, :customer, :teller, :amount, :account_type, :receiver, :message
   def initialize(fields)
     @type = fields[:type]
     @customer = fields[:customer]
     @teller = fields[:teller]
     @amount = fields[:amount]
     @account_type = fields[:account_type]
+    @receiver = fields[:receiver] if fields[:receiver] 
   end
 
   def perform_transaction
+    @oldBalance = @customer.balance(@account_type)
     if @type == "deposit"
-      @oldBalance = @customer.balance(@account_type)
       @customer.account[@account_type].deposit_funds(@amount)
     elsif @type == "withdrawal"
-      @oldBalance = @customer.balance(@account_type)
       @customer.account[@account_type].withdraw_funds(@amount)
+    elsif @type == "transfer"
+      @customer.account[@account_type].withdraw_funds(@amount)
+      @receiver.account[@account_type].deposit_funds(@amount)
     end
   end
 
-  def message
-    message = <<~MESSAGE
-      #{@type.capitalize} of GHC #{@amount.to_f} on #{@customer.first_name} #{@account_type} account 
-      completed by #{@teller.first_name}. New account balance : GHC #{@customer.balance(@account_type).to_f}
-    MESSAGE
-    return message
+  def generate_message
+    if @type == "deposit" or @type == "withdrawal"
+      action = "into"
+      action = "from" if @type == "withdrawal"
+      @message = <<~MESSAGE
+        #{@type.capitalize} of GHC #{@amount.to_f} #{action} #{@customer.first_name}'s #{@account_type} account completed by #{@teller.first_name}. 
+        Previous balance : GHC #{@oldBalance}
+        New balance : GHC #{@customer.balance(@account_type).to_f}
+      MESSAGE
+    elsif @type == "transfer"
+      @message = <<~MESSAGE
+        GHC #{@amount.to_f} transferred from #{@customer.first_name} to #{@receiver.first_name}'s #{@account_type} account completed by #{@teller.first_name}. 
+        Previous balance : GHC #{@oldBalance}
+        New balance : GHC #{@customer.balance(@account_type).to_f}
+      MESSAGE
+    end
   end
 end
+
+
